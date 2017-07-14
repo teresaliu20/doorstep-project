9286514d5a1c13ac379cc314a66f9e1b04a61acd
oadeboye <oadeboye@wellesley.edu>
2017-07-14 14:11:29 -0700

    Add item component

---
+++ frontend/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Item = ({}) => {
+    return(
+      <div>
+        <div>
+          <img />
+          image goes here
+        </div>
+        <div> description, item name, poster name go here</div>
+      </div>
+    )
+};
+
+export default Item;
--- frontend/components/Marketplace.js
+++ frontend/components/Marketplace.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import Item from './Item';
 
 const Marketplace = () => {
     return (
