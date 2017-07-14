eeb5e65f83221ff23a4b3865707d44b277c8eead
teresaliu20 <teresali@usc.edu>
2017-07-14 14:10:12 -0700

    navbar styling

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -6,9 +6,12 @@ import styles from '../assets/stylesheets/navbar.scss'
 const Navbar = () => {
     return (
         <div className="navbar">
+            <img src="https://image.flaticon.com/icons/png/512/343/343647.png"/>
             <h1>Doorstep</h1>
-            <Button bsStyle="primary">Dashboard</Button>
-            <Button>Profile</Button>
+            <div>
+              <button className="dashboard-button">Dashboard</button>
+              <button className="profile-button">Profile</button>
+            </div>
         </div>
     );
 };
