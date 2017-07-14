8b476481b3e8bf6f6547f86d3d9b296a7c454fad
teresaliu20 <teresali@usc.edu>
2017-07-14 14:26:22 -0700

    updated scss files

--- frontend/components/Neighbors.js
+++ frontend/components/Neighbors.js
@@ -5,7 +5,7 @@ import users from '../users';
 
 const Neighbors = ({}) => {
     return (
-        <div>
+        <div className="neighbors-div">
          {
              users.map(user =>
                  <Neighbor user={user}/>
