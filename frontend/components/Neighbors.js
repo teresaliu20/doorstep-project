bc9c9a42ec81e6aa94f0fa71a94aa17d2582ddbb
teresaliu20 <teresali@usc.edu>
2017-07-14 23:08:56 -0700

    Added neighbors button

--- frontend/components/Neighbors.js
+++ frontend/components/Neighbors.js
@@ -8,11 +8,15 @@ class Neighbors extends React.Component {
     render() {
         return (
             <div className="neighbors">
+                <h2>Neighbors</h2>
+                <div className="add-neighbor-button">Add a friend</div>
+                <div>
              {
                  this.props.users.map(user =>
                      <Neighbor user={user}/>
                  )
              }
+                </div>
             </div>
         );
     }
