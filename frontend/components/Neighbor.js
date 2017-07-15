5ed17f76fdd97009b40f3b369dba751205897df3
Minh To <to.nguyet.minh@gmail.com>
2017-07-15 02:35:29 -0700

    fixed routte

--- frontend/components/Neighbor.js
+++ frontend/components/Neighbor.js
@@ -8,12 +8,12 @@ import Profile from './Profile';
 class Neighbor extends React.Component {
     render() {
         return (
-                <div>
-                    <Link className="neighbor" to={'profile/' + this.props.user._id}>
-                        <span><img src={this.props.user.imgURL} alt="" height="40"/></span>
-                        <span>{this.props.user.fName} {this.props.user.lName}</span>
-                    </Link>
-                </div>
+            <div>
+                <Link className="neighbor" to={'/profile/' + this.props.user._id}>
+                    <span><img src={this.props.user.imgURL} alt="" height="40"/></span>
+                    <span>{this.props.user.fName} {this.props.user.lName}</span>
+                </Link>
+            </div>
         );
     }
 }
