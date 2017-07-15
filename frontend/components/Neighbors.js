1bea4ed46360bdd379af32e54fa2c51880a0da46
teresaliu20 <teresali@usc.edu>
2017-07-15 00:01:35 -0700

    fixed merge

--- frontend/components/Neighbors.js
+++ frontend/components/Neighbors.js
@@ -8,14 +8,6 @@ class Neighbors extends React.Component {
     render() {
         return (
             <div className="neighbors">
-                <h2>Neighbors</h2>
-                <div className="add-neighbor-button">Add a neighbor</div>
-                <div>
-             {
-                 this.props.users.map(user =>
-                     <Neighbor user={user}/>
-                 )
-             }
                 <div className="side-bar"></div>
                 <div className="neighbors-bar">
                     <h2>Neighbors</h2>
@@ -27,7 +19,6 @@ class Neighbors extends React.Component {
                      )
                  }
                     </div>
->>>>>>> 2f9a8f4372bf8548a8ea107b7dbb754912d534f3
                 </div>
             </div>
         );
