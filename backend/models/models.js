ff3dd138040f76aef2d4ada6f177b62deb7c75d2
oadeboye <oadeboye@wellesley.edu>
2017-07-14 17:08:02 -0700

    Add mongoose models for User, Item, Community

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,1 +1,59 @@
 const mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+    fName: {
+        type: String,
+    },
+    lName: {
+        type: String,
+    },
+    imgURL: {
+        type: String,
+    },
+});
+
+var itemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    // categories: {
+    //     type: Array,
+    // },
+    imgURL: {
+        type: String,
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+    },
+});
+
+var communitySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String
+    },
+    users: {
+        type: Array,
+    },
+    items: {
+        type: Array,
+    }
+});
+
+var User = mongoose.model('User', userSchema);
+var Item = mongoose.model('Item', itemSchema);
+var Community = mongoose.model('Community', communitySchema);
+
+export { User, Item, Community };
